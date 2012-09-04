7fd60f56e46848111064b2c6ab282272142b3446
John Hartley <fred@fred.com>
2012-09-05 08:06:58 +1000

    Version 0.1

--- NameMaker/NameMaker/Program.cs
+++ NameMaker/NameMaker/Program.cs
@@ -16,19 +16,35 @@ namespace NameMaker
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
+			if(args.Length == 0)
+			{
+				help();
+				return;
+			}
+			
+			int count=0;
+			// Scan Arguments
+			if(args[0].ToUpper().StartsWith("/C:"))
+			{
+				string countStr = args[0].Substring(3);
+				Int32.TryParse(countStr,out count);
+			}
 			
 			NameRandomiser nameMaker = new NameRandomiser();
 			string [] name;
-			for(int i=0;i<100;i++)
+			for(int i=0; i < count; i++)
 			{
 				name = nameMaker.randomName(NameRandomiser.Gender.either);
 				Console.WriteLine(name[0]+" "+name[1]);
 			}
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
 		}
-		
+		public static void help()
+		{
+			Console.WriteLine("Name Maker");
+			Console.WriteLine("");
+			Console.WriteLine("NAMEMAKER /C:runs");
+		}
 	}
 }
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,9 @@
-The NameMaker project is designed to generate random names.
+The NameMaker project is designed to generate random names to use for testing.
+
+
+
+Version 0.1, 5 September 2012
+* Command line switch /c:n generates n names in the format [FirstName][space][lastname]
+* Help added
+
+4 September 2012	First Release.
